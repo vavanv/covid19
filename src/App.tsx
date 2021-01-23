11b040c39375a802d74402a14658793e00298448
Vladimir V <vavan.v@gmail.com>
2021-01-23 10:55:00 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
 import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
-import "./App.css";
-import 'leaflet/dist/leaflet.css';
+// import { MapContainer, TileLayer } from "react-leaflet";
+// import "./App.css";
+// import "leaflet/dist/leaflet.css";
+// import "../node_modules/mapbox-gl/dist/mapbox-gl.css";
+import ReactMapGL from "react-map-gl";
+
+// import { styles } from "./styles";
+import { API_KEY } from "./constants";
+
+// function App2() {
+//   return (
+//     <MapContainer center={[0, 0]} zoom={3.3}>
+//       <TileLayer
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//       ></TileLayer>
+//     </MapContainer>
+//   );
+// }
 
 function App() {
+  const [viewport, setViewport] = React.useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  });
+
   return (
-    <MapContainer center={[43.653225, -79.383186]} zoom={12}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      ></TileLayer>
-    </MapContainer>
+    <ReactMapGL
+      {...viewport}
+      width="100%"
+      height="100%"
+      mapboxApiAccessToken={
+        "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA"
+      }
+      onViewportChange={(viewport) => setViewport(viewport)}
+      mapStyle="mapbox://styles/mapbox/light-v10"
+      // className={classes.map}
+    />
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Support Babel 7.4
+import "core-js/stable";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import * as React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "normalize.css";
+import App from "./App";
+import "typeface-roboto";
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
