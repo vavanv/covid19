1e6c955383b8c17d865721c1b63da9aef767b68b
Vladimir V <vavan.v@gmail.com>
2021-02-02 15:56:16 -0500

    add diff map

--- src/components/global-map/index.tsx
+++ src/components/global-map/index.tsx
@@ -22,7 +22,9 @@ const MapComponent = (props: Props & WithStyles<typeof styles>) => {
       <LeafletMap>
         <ChangeMap center={[0, 0]} zoom={2.9} />
         <TileLayer
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+          // url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+          // url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         ></TileLayer>
       </LeafletMap>
