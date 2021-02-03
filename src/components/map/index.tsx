73cc9651f471f934a2c7fa64c33a56d908e6da6a
Vladimir V <vavan.v@gmail.com>
2021-02-03 12:44:31 -0500

    change map style

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -26,6 +26,7 @@ const MapComponent = (props: Props & WithStyles<typeof styles>) => {
         <ChangeMap center={center} zoom={zoom} />
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           // url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
           // url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}.png"
           // url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
