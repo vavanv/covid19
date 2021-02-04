63381e244ab3499f90ccb6270f8e377d3fa8d3e5
Vladimir V <vavan.v@gmail.com>
2021-02-04 12:41:42 -0500

    add component for styles radio buttons

--- src/components/type-selector/index.tsx
+++ src/components/type-selector/index.tsx
@@ -1,43 +1,13 @@
 import * as React from 'react';
-import { FormControl, RadioGroup, FormControlLabel, Radio } from '@material-ui/core/';
+import { FormControl, RadioGroup, FormControlLabel } from '@material-ui/core/';
 import { withStyles, WithStyles } from '@material-ui/core';
 
-import { cases_color, recovered_color, deaths_color } from '../../assets/jss/portal-material';
+import { CasesRadio, RecoveredRadio, DeathsRadio } from './radio-buttons';
 import { styles } from './styles';
 
 interface Props extends WithStyles<typeof styles> {
   type: string;
 }
-
-const CasesRadio = withStyles({
-  root: {
-    color: cases_color,
-    '&$checked': {
-      color: cases_color,
-    },
-  },
-  checked: {},
-})(props => <Radio color="default" {...props} size="small" />);
-
-const RecoveredRadio = withStyles({
-  root: {
-    color: recovered_color,
-    '&$checked': {
-      color: recovered_color,
-    },
-  },
-  checked: {},
-})(props => <Radio color="default" {...props} size="small" />);
-
-const DeathsRadio = withStyles({
-  root: {
-    color: deaths_color,
-    '&$checked': {
-      color: deaths_color,
-    },
-  },
-  checked: {},
-})(props => <Radio color="default" {...props} size="small" />);
 
 const TypeSelectorComponent = (props: Props & WithStyles<typeof styles>) => {
   const { classes, type } = props;
---
+++ src/components/type-selector/radio-buttons/index.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { withStyles } from '@material-ui/core';
+import { Radio } from '@material-ui/core/';
+
+import { cases_color, recovered_color, deaths_color } from '../../../assets/jss/portal-material';
+
+export const CasesRadio = withStyles({
+  root: {
+    color: cases_color,
+    '&$checked': {
+      color: cases_color,
+    },
+  },
+  checked: {},
+})(props => <Radio color="default" {...props} size="small" />);
+
+export const RecoveredRadio = withStyles({
+  root: {
+    color: recovered_color,
+    '&$checked': {
+      color: recovered_color,
+    },
+  },
+  checked: {},
+})(props => <Radio color="default" {...props} size="small" />);
+
+export const DeathsRadio = withStyles({
+  root: {
+    color: deaths_color,
+    '&$checked': {
+      color: deaths_color,
+    },
+  },
+  checked: {},
+})(props => <Radio color="default" {...props} size="small" />);
