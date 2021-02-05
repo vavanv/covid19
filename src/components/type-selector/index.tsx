9f3a3fa0a70aeca9b3f8514972e76e36cfcc0f7c
Vladimir V <vavan.v@gmail.com>
2021-02-05 15:10:42 -0500

    in progress

--- src/components/type-selector/index.tsx
+++ src/components/type-selector/index.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import { FormControl, RadioGroup, FormControlLabel } from '@material-ui/core/';
 import { withStyles, WithStyles } from '@material-ui/core';
 
-import { CasesRadio, RecoveredRadio, DeathsRadio } from './radio-buttons';
+// import { CasesRadio, RecoveredRadio, DeathsRadio } from './radio-buttons';
+import { cases_color, recovered_color, deaths_color } from '../../assets/jss/portal-material';
+import { RadioButton } from './radio-buttons';
 import { styles } from './styles';
 
 interface Props extends WithStyles<typeof styles> {
@@ -16,20 +18,17 @@ const TypeSelectorComponent = (props: Props & WithStyles<typeof styles>) => {
     <FormControl component="fieldset" className={classes.form}>
       <RadioGroup row aria-label="position" name="position" defaultValue={type}>
         <FormControlLabel
-          value="cases"
-          control={<CasesRadio />}
+          control={<RadioButton type="cases" color={cases_color} />}
           label="Cases"
           labelPlacement="top"
         />
         <FormControlLabel
-          value="recovered"
-          control={<RecoveredRadio />}
+          control={<RadioButton type="recovered" color={recovered_color} />}
           label="Recovered"
           labelPlacement="top"
         />
         <FormControlLabel
-          value="deaths"
-          control={<DeathsRadio />}
+          control={<RadioButton type="deaths" color={deaths_color} />}
           label="Deaths"
           labelPlacement="top"
         />
--- src/components/type-selector/radio-buttons/styles.tsx
+++ src/components/type-selector/radio-buttons/styles.tsx
@@ -1,26 +1,51 @@
-import { createStyles } from '@material-ui/core';
-import { cases_color, recovered_color, deaths_color } from '../../../assets/jss/portal-material';
+import React from 'react';
+// // import { createStyles } from '@material-ui/core';
+// // import { cases_color, recovered_color, deaths_color } from '../../../assets/jss/portal-material';
 
-export const styles = () => {
-  return createStyles({
-    cases: {
-      color: cases_color,
-      '&$checked': {
-        color: cases_color,
-      },
-    },
-    recovered: {
-      color: recovered_color,
-      '&$checked': {
-        color: recovered_color,
-      },
-    },
-    deaths: {
-      color: deaths_color,
-      '&$checked': {
-        color: deaths_color,
-      },
-    },
-    checked: {},
-  });
-};
+// // export const styles = () => {
+// //   return createStyles({
+// //     root: {
+// //       color: cases_color,
+// //       '&$checked': {
+// //         color: cases_color,
+// //       },
+// //     },
+// //     cases: {
+// //       color: cases_color,
+// //       '&$checked': {
+// //         color: cases_color,
+// //       },
+// //     },
+// //     recovered: {
+// //       color: recovered_color,
+// //       '&$checked': {
+// //         color: recovered_color,
+// //       },
+// //     },
+// //     deaths: {
+// //       color: deaths_color,
+// //       '&$checked': {
+// //         color: deaths_color,
+// //       },
+// //     },
+// //     checked: {},
+// //   });
+// // };
+
+// import React from 'react';
+// import { makeStyles } from '@material-ui/styles';
+// const useStyles = makeStyles({
+//   root: {
+//     color: ({ color }) => `${color}`,
+//     '&$checked': {
+//       color: ({ color }) => `${color}`,
+//     },
+//   },
+// });
+
+// const StyledComponent = ({ component: ComponentProp = 'div', children, color, hoverColor }) => {
+//   const classes = useStyles({ color, hoverColor });
+//   return <ComponentProp className={classes.root}>{children}</ComponentProp>;
+// };
+
+// export default StyledComponent;
