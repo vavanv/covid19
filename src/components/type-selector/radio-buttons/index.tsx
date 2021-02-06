c364d4a10a6e3acc05e7c7c4072c82f3d02656e3
Vladimir V <vavan.v@gmail.com>
2021-02-05 20:57:55 -0500

    clean up Radio button control

--- src/components/type-selector/radio-buttons/index.tsx
+++ src/components/type-selector/radio-buttons/index.tsx
@@ -1,26 +1,12 @@
 import * as React from 'react';
 // import { useDispatch } from 'react-redux';
-// import { makeStyles, withStyles, WithStyles } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core';
 import { Radio } from '@material-ui/core/';
 
-// import { cases_color, recovered_color, deaths_color } from '../../../assets/jss/portal-material';
-// import { styles } from './styles';
+import { useStyles } from './styles';
 
 const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
   // var a = event.target.value;
 };
-
-const useStyles = makeStyles((color: string) => ({
-  root: {
-    color: `${color}`,
-    '&$checked': {
-      color: `${color}`,
-    },
-  },
-  checked: {},
-}));
-
 interface Props {
   color: string;
   type: string;
@@ -28,48 +14,11 @@ interface Props {
 
 const RadioButtonComponent = (props: Props) => {
   const { color, type } = props;
-  const classes = useStyles({ color });
-  return <Radio size="small" onChange={handleChange} className={classes.root} value={type} />;
+  const classes = useStyles({
+    color: color,
+  });
+
+  return <Radio size="small" onChange={handleChange} classes={classes} value={type} />;
 };
 
 export const RadioButton = React.memo(RadioButtonComponent);
-
-// export const RadioButtonComponent = withStyles({
-//   root: {
-//     color: cases_color,
-//     '&$checked': {
-//       color: cases_color,
-//     },
-//   },
-//   checked: {},
-// })(props => <Radio {...props} size="small" onChange={handleChange} />);
-
-// export const CasesRadio = withStyles({
-//   root: {
-//     color: cases_color,
-//     '&$checked': {
-//       color: cases_color,
-//     },
-//   },
-//   checked: {},
-// })(props => <Radio {...props} size="small" onChange={handleChange} />);
-
-// export const RecoveredRadio = withStyles({
-//   root: {
-//     color: recovered_color,
-//     '&$checked': {
-//       color: recovered_color,
-//     },
-//   },
-//   checked: {},
-// })(props => <Radio {...props} size="small" onChange={handleChange} />);
-
-// export const DeathsRadio = withStyles({
-//   root: {
-//     color: deaths_color,
-//     '&$checked': {
-//       color: deaths_color,
-//     },
-//   },
-//   checked: {},
-// })(props => <Radio {...props} size="small" onChange={handleChange} />);
--- src/components/type-selector/radio-buttons/styles.tsx
+++ src/components/type-selector/radio-buttons/styles.tsx
@@ -1,51 +1,17 @@
-import React from 'react';
-// // import { createStyles } from '@material-ui/core';
-// // import { cases_color, recovered_color, deaths_color } from '../../../assets/jss/portal-material';
+import { makeStyles } from '@material-ui/styles';
 
-// // export const styles = () => {
-// //   return createStyles({
-// //     root: {
-// //       color: cases_color,
-// //       '&$checked': {
-// //         color: cases_color,
-// //       },
-// //     },
-// //     cases: {
-// //       color: cases_color,
-// //       '&$checked': {
-// //         color: cases_color,
-// //       },
-// //     },
-// //     recovered: {
-// //       color: recovered_color,
-// //       '&$checked': {
-// //         color: recovered_color,
-// //       },
-// //     },
-// //     deaths: {
-// //       color: deaths_color,
-// //       '&$checked': {
-// //         color: deaths_color,
-// //       },
-// //     },
-// //     checked: {},
-// //   });
-// // };
+interface RadioButtonStyle {
+  color: string;
+}
 
-// import React from 'react';
-// import { makeStyles } from '@material-ui/styles';
-// const useStyles = makeStyles({
-//   root: {
-//     color: ({ color }) => `${color}`,
-//     '&$checked': {
-//       color: ({ color }) => `${color}`,
-//     },
-//   },
-// });
-
-// const StyledComponent = ({ component: ComponentProp = 'div', children, color, hoverColor }) => {
-//   const classes = useStyles({ color, hoverColor });
-//   return <ComponentProp className={classes.root}>{children}</ComponentProp>;
-// };
-
-// export default StyledComponent;
+export const useStyles = makeStyles({
+  root: (props: RadioButtonStyle) => ({
+    color: props.color,
+    // '&$checked': {
+    //   color: props.color,
+    // },
+    // checked: {
+    //   color: props.color,
+    // },
+  }),
+});
