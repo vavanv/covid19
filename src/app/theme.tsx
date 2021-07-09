402ee11873d2c06a0afa679f8d44db6b35eacd37
Vladimir V <vavan.v@gmail.com>
2021-07-09 11:09:29 -0400

    add responsive font size

--- src/app/theme.tsx
+++ src/app/theme.tsx
@@ -1,4 +1,4 @@
-import { createMuiTheme,responsiveFontSizes } from '@material-ui/core';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core';
 
 const theme = createMuiTheme({
   palette: {
